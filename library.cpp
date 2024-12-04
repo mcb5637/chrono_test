b67f8d4466dbd9afa3323912abe7874569ab9930
mcb <mgrbo@web.de>
2024-12-04 13:56:00 +0100

    call once

--- library.cpp
+++ library.cpp
@@ -1,7 +1,8 @@
 #include "library.h"
 
-#include <iostream>
 #include <chrono>
+#include <iostream>
+#include <mutex>
 
 void hello() {
     auto n = std::chrono::system_clock::now();
@@ -9,4 +10,10 @@ void hello() {
     std::chrono::zoned_time ny(std::chrono::get_tzdb().locate_zone("America/New_York"), n);
 
     std::cout << std::format("n {}\n current {}\n ny {}\n", n, current, ny);
+
+    static std::once_flag f{};
+
+    std::call_once(f, []() {
+        std::cout << "called once";
+    });
 }
